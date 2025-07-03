77ff1f3bfcebe7b846336d3c787c5a2ed63ddf5b
Aditya Prem <aditya.prem2003@gmail.com>
2025-07-03 23:32:05 +0530

    done routing for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import CategoryCarousel from './components/CategoryCarousel'
-import FlashDeals from './components/FlashDeals'
-import SeasonalSection from './components/SeasonalSection'
-import HomeDecorSection from './components/HomeDecorSection'
-import VideoSection from './components/VideoSection'
-import TrendingSection from './components/TrendingSection'
-import Footer from './components/Footer'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import other pages later
 
-function App() {
+const App = () => {
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <main>
-        <Hero />
-        <CategoryCarousel />
-        <FlashDeals />
-        <SeasonalSection />
-        <HomeDecorSection />
-        <VideoSection />
-        <TrendingSection />
-      </main>
-      <Footer />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/products" element={<Products />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
