24880de9dd686a2c475725073e37883ba3be55fe
riyo264 <supriyodhani50@gmail.com>
2026-01-25 22:58:49 +0530

    server file changes

--- Backend/SpeechAssistant/server.py
+++ Backend/SpeechAssistant/server.py
@@ -1,50 +1,38 @@
 import os
-from livekit import api
 from flask import Flask, request
-from dotenv import load_dotenv
 from flask_cors import CORS
-from livekit.api import LiveKitAPI, ListRoomsRequest
-import uuid
-import asyncio
+from dotenv import load_dotenv
+from livekit import api
 
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
-async def generate_room_name():
-    name = "room-" + str(uuid.uuid4())[:8]
-    rooms = await get_rooms()
-    while name in rooms:
-        name = "room-" + str(uuid.uuid4())[:8]
-    return name
-
-async def get_rooms():
-    api = LiveKitAPI()
-    rooms = await api.room.list_rooms(ListRoomsRequest())
-    await api.aclose()
-    return [room.name for room in rooms.rooms]
+CORS(app)
 
 @app.route("/getToken")
 def get_token():
-    name = request.args.get("name", "my name")
-    room = request.args.get("room", None)
-    
-    if not room:
-        rooms = asyncio.run(get_rooms())
-        
-    token = api.AccessToken(os.getenv("LIVEKIT_API_KEY"), os.getenv("LIVEKIT_API_SECRET")) \
-        .with_identity(name)\
-        .with_name(name)\
-        .with_grants(api.VideoGrants(
-            room_join=True,
-            room=room
-        ))
-    
+    name = request.args.get("name", "guest")
+    room = request.args.get("room", "default-room")
+
+    token = (
+        api.AccessToken(
+            os.environ["LIVEKIT_API_KEY"],
+            os.environ["LIVEKIT_API_SECRET"],
+        )
+        .with_identity(name)
+        .with_name(name)
+        .with_grants(
+            api.VideoGrants(
+                room_join=True,
+                room=room,
+            )
+        )
+    )
+
     return token.to_jwt()
 
 if __name__ == "__main__":
     app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 8080)),
-)
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+    )
